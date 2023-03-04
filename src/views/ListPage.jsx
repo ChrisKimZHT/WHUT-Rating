6cbbeb89b9b498615ad7a7419b407b802d3d4d89
ChrisKim <1515748548@qq.com>
2023-03-04 16:13:23 +0800

    [feat]: 到底自动刷新

--- src/views/ListPage.jsx
+++ src/views/ListPage.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import ShopList from '../components/ShopList';
 import IndexMenu from '../components/IndexMenu/IndexMenu';
+import Fab from '@mui/material/Fab';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import { service } from '../service/Service';
+import { display } from '@mui/system';
 
 const ListPage = () => {
   const [regionId, setRegionId] = useState("");
@@ -9,6 +12,8 @@ const ListPage = () => {
   const [optionId, setOptionId] = useState("");
   const [methodId, setMethodId] = useState("");
   const [shopData, setShopData] = useState([]);
+  const [curPage, setCurPage] = useState(0);
+  const [scroll, setScroll] = useState(0);
 
   useEffect(() => {
     if (!regionId || !typeId || !optionId || !methodId) return;
@@ -23,6 +28,35 @@ const ListPage = () => {
     };
     refreshShopData();
   }, [regionId, typeId, optionId, methodId]);
+
+  useEffect(() => {
+    setCurPage(0);
+    window.scrollTo(0, 0);
+  }, [typeId]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+      setScroll(scrollTop);
+      if (Math.abs(scrollTop + clientHeight - scrollHeight) <= 1) {
+        service.list.board(regionId, typeId, optionId, methodId, curPage + 1)
+          .then(res => {
+            if (res.data.shops.length === 0) return;
+            setShopData(shopData.concat(res.data.shops));
+            setCurPage(curPage + 1);
+          })
+          .catch(err => {
+            console.log("/list/board: " + err);
+          });
+      }
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [regionId, typeId, optionId, methodId, shopData, curPage]);
+
+  const toTop = () => {
+    window.scrollTo(0, 0);
+  }
 
   return (
     <div>
@@ -39,6 +73,19 @@ const ListPage = () => {
       <ShopList
         shopData={shopData}
       />
+      <Fab
+        color="primary"
+        style={{
+          position: 'fixed',
+          bottom: '70px',
+          right: '10px',
+          opacity: scroll > 100 ? 1 : 0,
+          transition: "opacity 225ms",
+        }}
+        onClick={toTop}
+      >
+        <KeyboardArrowUpIcon />
+      </Fab>
     </div>
   );
 }
